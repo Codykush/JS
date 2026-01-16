64466da5a7833485cb991936869444e730190b42
Kushagra Singh Bhadauria <156601936+Codykush@users.noreply.github.com>
2026-01-16 09:51:24 +0000

    Basic Datatypes

---
+++ Baiscs_01/01_Variables.js
@@ -0,0 +1,17 @@
+const ID = "123"
+let name = "Kushagra"
+var city = "Mumbai"
+let Salary;
+
+/*
+var is not allowed in block scope and function scope
+let and const are allowed in block scope but not in function scope
+
+*/
+
+city = "GreaterNoida"
+name = "KSB"
+
+
+console.log(ID)
+console.table([ID,name,city,Salary])
