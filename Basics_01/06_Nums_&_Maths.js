94687a4766e572f89b21953a8408b6653238b3b1
Kushagra Singh Bhadauria <156601936+Codykush@users.noreply.github.com>
2026-01-19 02:13:52 +0530

    Add various math operations and number formatting

--- Basics_01/06_Nums_&_Maths.js
+++ Basics_01/06_Nums_&_Maths.js
@@ -3,3 +3,30 @@ console.log(age)
 
 const num = new Number(100)
 console.log(num)
+
+console.log(num.toString().length)
+console.log(num.toFixed(2))
+
+const newNum = 23.456465
+console.log(newNum.toPrecision(3))
+
+const rupees = 1000000
+console.log(rupees.toLocaleString('en-In'))
+
+// MATHS
+
+console.log(Math)
+console.log(Math.abs(-5))
+console.log(Math.round(4.5))
+console.log(Math.ceil(4.9))
+console.log(Math.floor(4.9))
+console.log(Math.min(4,5,8,1))
+console.log(Math.max(4,5,8,1))
+
+console.log(Math.random())
+console.log((Math.random()*10)+1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()* (max - min + 1) * min))
